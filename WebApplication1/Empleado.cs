d7713896739694f3e51c08afb3120bf8e9382971
Patricio Scidá <patricioscida@gmail.com>
2021-08-30 15:13:19 -0300

    Definida la clase Empleado.

--- WebApplication1/Empleado.cs
+++ WebApplication1/Empleado.cs
@@ -15,20 +15,20 @@ namespace WebApplication1 {
         public int Antiguedad { get => antiguedad; set => antiguedad = value; }
         public float Salario { get => salario; set => salario = value; }
 
-        static Empleado crearEmpleado(string nombre, string apellido, int edad, int antiguedad, float salario) {
+        static Empleado CrearEmpleado(string nombre, string apellido, int edad, int antiguedad) {
             var nuevoEmpleado = new Empleado();
             var rand = new Random();
             int sueldoBasico = rand.Next(17000, 80000);
-            double adicional = calcularAdicional(antiguedad, sueldoBasico);
+            double adicional = CalcularAdicional(antiguedad, sueldoBasico);
             nuevoEmpleado.Nombre = nombre;
             nuevoEmpleado.Apellido = apellido;
             nuevoEmpleado.Edad = edad;
             nuevoEmpleado.Antiguedad = antiguedad;
-            nuevoEmpleado.Salario = salario;
+            nuevoEmpleado.Salario = (float)CalcularSalario(adicional, sueldoBasico);
             return nuevoEmpleado;
         }
 
-        public static double calcularAdicional(int antiguedad, int sueldoBasico) {
+        public static double CalcularAdicional(int antiguedad, int sueldoBasico) {
             // Adicional = 1% del sueldo básico por cada año, es 25% a partir de 20 años.
             double porcentaje;
             if (antiguedad < 20) {
@@ -39,7 +39,7 @@ namespace WebApplication1 {
             return sueldoBasico * porcentaje;
         }
 
-        public static double calcularSueldo(double adicional, int sueldoBasico, double descuento) {
+        public static double CalcularSalario(double adicional, int sueldoBasico) {
             double sueldo = sueldoBasico + adicional - (sueldoBasico * 0.15);
             return sueldo;
         }
